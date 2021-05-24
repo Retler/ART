c21b9783e7b6cda936adf0919c09d52b086c650a
Sergej Roswall Bogachov <sergej@cyber-crypt.com>
2021-05-24 21:58:53 +0200

    now serving tweets on statsapi

--- cmd/statsapi/main.go
+++ cmd/statsapi/main.go
@@ -15,6 +15,7 @@ func handleRequests() {
 	myRouter := mux.NewRouter().StrictSlash(true)
 	myRouter.HandleFunc("/", handlers.Home)
 	myRouter.HandleFunc("/tweet", handlers.GetTweet)
+	myRouter.HandleFunc("/tweets", handlers.GetTweetsSince)
 	
 	log.Fatal(http.ListenAndServe(":10000", myRouter))
 }
