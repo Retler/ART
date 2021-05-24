e659956845a7dcae584a50f75b705e1b6fd968df
Sergej Roswall Bogachov <sergej@cyber-crypt.com>
2021-05-24 21:58:53 +0200

    now serving tweets on statsapi

--- statshandlers/statshandlers.go
+++ statshandlers/statshandlers.go
@@ -2,6 +2,8 @@ package statshandlers
 
 import(
 	"fmt"
+	"time"
+	"strconv"
 	"net/http"
 	"encoding/json"
 	repo "github.com/Retler/ART/tweet_repo"
@@ -11,6 +13,7 @@ func Home(w http.ResponseWriter, r *http.Request){
 	fmt.Fprintln(w, "ART StatsAPI")
 }
 
+// TODO: Change API endpoints to serve stats rather than tweets
 func GetTweet(w http.ResponseWriter, r *http.Request){
 	tweets := repo.NewMemoryRepoMock()
 	keys, ok := r.URL.Query()["tweet_id"]
@@ -29,4 +32,36 @@ func GetTweet(w http.ResponseWriter, r *http.Request){
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(tweet)
+}
+
+func GetTweetsSince(w http.ResponseWriter, r *http.Request){
+	tweets := repo.NewMemoryRepoMock()
+	keys, ok := r.URL.Query()["age_minutes"]
+	if !ok || len(keys[0]) < 1 {
+		w.WriteHeader(400)
+		return
+	}
+
+	ageMinutesStr := keys[0]
+	ageMinutes, err := strconv.Atoi(ageMinutesStr)
+	if err != nil{
+		w.WriteHeader(404)
+		return
+	}
+	
+	ageOfTweets := time.Now().UTC().Add(-time.Duration(ageMinutes) * time.Minute)
+	tweetsSince, err := tweets.GetTweetsSince(ageOfTweets)
+	if err != nil{
+		w.WriteHeader(500)
+		return
+	}
+
+	if len(tweetsSince) == 0{
+		w.WriteHeader(404)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(tweetsSince)
 }
